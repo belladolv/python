d726b117c89b7ff3373a7224b5c01e67085c7425
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-13 17:02:27 -0300

    Create atv90.py

---
+++ atv90.py
@@ -0,0 +1,9 @@
+def Somador(a, b):
+    print(f"A soma de {a} e {b} é {a + b}")
+
+# Leitura dos dados
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+
+# Chamada da função
+Somador(a, b)
