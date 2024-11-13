789697ad1acc49f7e2de4979cef47d20943431dd
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-13 17:01:13 -0300

    Update atv88.py

--- atv88.py
+++ atv88.py
@@ -1,1 +1,9 @@
+def Gerador(mensagem, repeticoes):
+    for _ in range(repeticoes):
+        print("+-------=======------+")
+        print(mensagem)
+        print("+-------=======------+")
+    
+# Chamada da função
+Gerador("Aprendendo Python", 4)
 
