b0a32c64053f14d821039fe8a082da47d7a39018
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-13 17:05:01 -0300

    Create atv95.py

---
+++ atv95.py
@@ -0,0 +1,10 @@
+def Somador(a, b):
+    return a + b
+
+# Leitura dos dados
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+
+# Chamada da função e exibição do resultado
+resultado = Somador(a, b)
+print(f"A soma de {a} e {b} é {resultado}")
