50531541fa789030eb2d20d7125a286cc5bc9639
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:37:14 -0300

    Update atv10.py

--- atv10.py
+++ atv10.py
@@ -4,3 +4,12 @@ area = largura * altura
 tinta_necessaria = area / 2
 print(f"A área da parede é {area} metros quadrados.")
 print(f"Você precisará de {tinta_necessaria} litros de tinta para pintá-la.")
+
+Digite a largura da parede em metros: 
+50
+Digite a altura da parede em metros: 
+100
+A área da parede é 5000.0 metros quadrados.
+Você precisará de 2500.0 litros de tinta para pintá-la.
+
+
