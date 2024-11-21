4c7e17085754019e7d2df9017d3c3cb5078e064f
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:39:08 -0300

    Update atv11.py

--- atv11.py
+++ atv11.py
@@ -3,3 +3,15 @@ B = float(input("Digite o valor de B: "))
 C = float(input("Digite o valor de C: "))
 delta = B**2 - 4 * A * C
 print(f"O valor de Delta é {delta}.")
+
+Digite o valor de A: 
+5
+Digite o valor de B: 
+7
+Digite o valor de C: 
+9
+O valor de Delta é -131.0.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
