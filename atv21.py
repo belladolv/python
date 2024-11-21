f8a18c19bcb5e85e9ba81d6e771253452338c0bf
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:46:02 -0300

    Update atv21.py

--- atv21.py
+++ atv21.py
@@ -4,3 +4,11 @@ if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print(f"{ano} é um ano BISSEXTO.")
 else:
     print(f"{ano} não é um ano BISSEXTO.")
+
+Digite um ano: 
+1980
+1980 é um ano BISSEXTO.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
