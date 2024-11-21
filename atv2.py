2e1416e42d73ccf348b9b52e6caecec2fb250cf7
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:44:31 -0300

    Update atv2.py

--- atv2.py
+++ atv2.py
@@ -1,2 +1,10 @@
 nome = input("Digite seu nome: ")
 print(f"É um prazer te conhecer, {nome}!")
+
+Digite seu nome: 
+izabella
+É um prazer te conhecer, izabella!
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
