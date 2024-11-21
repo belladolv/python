00eb85b7eb4690c6be5bbeca51c1a777c09e8dc2
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:42:10 -0300

    Update atv16.py

--- atv16.py
+++ atv16.py
@@ -5,3 +5,13 @@ total_cigarros = cigarros_por_dia * anos_fumando * 365
 total_minutos_perdidos = total_cigarros * minutos_perdidos_por_cigarro
 dias_perdidos = total_minutos_perdidos / (24 * 60)
 print(f"Você perdeu aproximadamente {dias_perdidos:.2f} dias de vida devido ao fumo.")
+
+Digite a quantidade de cigarros fumados por dia: 
+8
+Digite a quantidade de anos que você já fuma: 
+17
+Você perdeu aproximadamente 344.72 dias de vida devido ao fumo.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
