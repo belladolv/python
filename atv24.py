abd5ddaa34ea7be384239a1c6a0c286258ed6ccf
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:47:59 -0300

    Update atv24.py

--- atv24.py
+++ atv24.py
@@ -6,3 +6,11 @@ else:
     preco = distancia * 0.45
 
 print(f"O preço da passagem é R${preco:.2f}")
+
+Qual a distância da viagem em Km? 
+600
+O preço da passagem é R$270.00
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
