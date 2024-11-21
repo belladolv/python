ec051ec7d4f54efca5538cde1b2599d04d7875fe
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:41:38 -0300

    Update atv15.py

--- atv15.py
+++ atv15.py
@@ -3,3 +3,11 @@ horas_por_dia = 8
 salario_por_hora = 25
 salario_total = dias_trabalhados * horas_por_dia * salario_por_hora
 print(f"O salário total do mês é R${salario_total:.2f}.")
+
+Digite o número de dias trabalhados no mês: 
+12
+O salário total do mês é R$2400.00.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
