325994b4fcd998580d7388ee50828e24b1dceada
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:39:42 -0300

    Update atv12.py

--- atv12.py
+++ atv12.py
@@ -2,3 +2,11 @@ preco = float(input("Digite o preço do produto: R$"))
 desconto = preco * 0.05
 preco_promocional = preco - desconto
 print(f"O preço promocional com 5% de desconto é R${preco_promocional:.2f}.")
+
+Digite o preço do produto: R$
+150
+O preço promocional com 5% de desconto é R$142.50.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
