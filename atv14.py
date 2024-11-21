ce3d4be0c785b840125431aa7ba001517aa6167c
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:40:51 -0300

    Update atv14.py

--- atv14.py
+++ atv14.py
@@ -4,3 +4,13 @@ preco_km = km_percorridos * 0.20
 preco_dias = dias_alugados * 90
 preco_total = preco_km + preco_dias
 print(f"O preço total a pagar é R${preco_total:.2f}.")
+
+Digite a quantidade de Km percorridos: 
+500
+Digite a quantidade de dias pelos quais o carro foi alugado: 
+8
+O preço total a pagar é R$820.00.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
