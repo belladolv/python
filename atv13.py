a8dc79fc3c3bb07092b3eb53436ffd1e72bc315b
Izabella O. Santos <izabellaoliverasantos@gmail.com>
2024-11-21 14:40:17 -0300

    Update atv13.py

--- atv13.py
+++ atv13.py
@@ -2,3 +2,11 @@ salario = float(input("Digite o salário do funcionário: R$"))
 aumento = salario * 0.15
 novo_salario = salario + aumento
 print(f"O novo salário com 15% de aumento é R${novo_salario:.2f}.")
+
+Digite o salário do funcionário: R$
+6000
+O novo salário com 15% de aumento é R$6900.00.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
